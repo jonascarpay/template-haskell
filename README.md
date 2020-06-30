86f3df5beb1cf16c967fd336b51532848c3c7de2
Jonas <jonascarpay@gmail.com>
2020-07-01 02:52:26 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# NAME
+
+A template Haskell + Nix project.
+Uses `haskell.nix` to get a consistent `ormolu` + `ghcide` + `hlint` going.
---
+++ shell.nix
@@ -0,0 +1,14 @@
+let
+  unstable = import <unstable> { };
+  hsPkgs = import ./.;
+in hsPkgs.shellFor {
+  withHoogle = true;
+  tools = {
+    cabal = "3.2.0.0";
+    hlint = "3.1.5";
+    ghcid = "0.8.7";
+    ghcide = "0.2.0";
+    ormolu = "0.1.0.0";
+  };
+  exactDeps = true;
+}
