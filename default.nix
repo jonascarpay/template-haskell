372327ec69fe3e80ee4b5935ed8e676bcc57922e
Jonas Carpay <jonascarpay@gmail.com>
2020-11-22 06:13:58 +0900

    Bump haskell.nix

--- default.nix
+++ default.nix
@@ -1,8 +1,8 @@
 let
   haskellNixSrc = builtins.fetchTarball {
     url =
-      "https://github.com/input-output-hk/haskell.nix/archive/c583fb3e775d0f071b58a53f82f726cec623c800.tar.gz";
-    sha256 = "0pzsaxwap6jmmpcyzwvygbrfcanjzkwfrmlqac4jnjrfixd8g27s";
+      "https://github.com/input-output-hk/haskell.nix/archive/01003480bdee1717b95ca99049d283168b405925.tar.gz";
+    sha256 = "06j381kix405k3x8yqlccwqckjrc3qrdqchbbd80cd6slf6ybg9c";
   };
   haskellNix = import haskellNixSrc { };
   pkgsSrc = haskellNix.sources.nixpkgs-2003;
