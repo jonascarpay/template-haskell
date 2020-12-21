84ff971a659e76a66212ad7325a6b7a94cd2ca14
Jonas Carpay <jonascarpay@gmail.com>
2020-12-22 00:19:48 +0900

    tweak shell.nix

--- shell.nix
+++ shell.nix
@@ -1,20 +1,20 @@
 let
-  hsPkgs = import ./.;
-  pkgs = import <nixpkgs> { };
-  ormolu = hsPkgs.haskell-nix.tool hsPkgs.projectArgs.compiler-nix-name "ormolu" "0.1.4.1";
+  pkgs = import ./pkgs.nix;
+  hsPkgs = pkgs.hsPkgs;
+  ormolu = pkgs.haskell-nix.tool hsPkgs.projectArgs.compiler-nix-name "ormolu" "0.1.4.1";
+  # Warning: only works with ghc 8.10 and up!
+  ormolu-wrapped = pkgs.writeShellScriptBin "ormolu" ''
+    ${ormolu}/bin/ormolu --ghc-opt=-XImportQualifiedPost $@
+  '';
 in
 hsPkgs.shellFor {
   withHoogle = true;
   tools = {
     cabal = "3.2.0.0";
-    hlint = "3.2.2";
+    hlint = "3.2.3";
     ghcid = "0.8.7";
     haskell-language-server = "0.6.0";
   };
-  buildInputs = [
-    (pkgs.writeShellScriptBin "ormolu" ''
-      ormolu --ghc-opt=-XImportQualifiedPost $@
-    '')
-  ];
+  buildInputs = [ ormolu-wrapped ];
   exactDeps = true;
 }
