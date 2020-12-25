1fabdadd3aa09297c3e39bbecddceebec05f4713
Jonas Carpay <jonascarpay@gmail.com>
2020-12-26 05:09:04 +0900

    Add dummy test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,4 +1,12 @@
-import Lib
+import Test.Tasty
+import Test.Tasty.ExpectedFailure
+import Test.Tasty.HUnit
+import Test.Tasty.QuickCheck
 
 main :: IO ()
-main = putStrLn "heyyyyyyyyy"
+main =
+  defaultMain $
+    testGroup
+      "PKGNAME-test"
+      [ expectFail $ testCase "war is peace" $ 2 + 2 @?= (5 :: Int)
+      ]
