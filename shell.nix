d6f100026ab081979ba75e5cecf4de3845d1a30b
Jonas Carpay <jonascarpay@gmail.com>
2021-04-19 17:56:19 +0900

    expand note about wrapping ormolu

--- shell.nix
+++ shell.nix
@@ -1,14 +1,14 @@
-# To have ormolu automatically format imports using the ImportQualifiedPost syntax,
-# comment out `ormolu` from the `tools` section, and uncomment `buildInputs`
 let
   pkgs = import ./pkgs.nix;
   hsPkgs = pkgs.hsPkgs;
-  ormolu-wrapped =
-    let ormolu = pkgs.haskell-nix.tool hsPkgs.projectModule.compiler-nix-name "ormolu" "latest";
-    in
-    pkgs.writeShellScriptBin "ormolu" ''
-      ${ormolu}/bin/ormolu --ghc-opt=-XImportQualifiedPost $@
-    '';
+  ## To configure ormolu, I often end up creating a wrapped version in which I pass it the desired flags.
+  ## To use it, remove the ormolu line from the `tools` section, uncomment the lines below, and add `ormolu-wrapped` to  `shellFor`'s `buildInputs`.
+  # ormolu-wrapped =
+  #   let ormolu = pkgs.haskell-nix.tool hsPkgs.projectModule.compiler-nix-name "ormolu" "latest";
+  #   in
+  #   pkgs.writeShellScriptBin "ormolu" ''
+  #     ${ormolu}/bin/ormolu --ghc-opt=-XImportQualifiedPost $@
+  #   '';
 in
 hsPkgs.shellFor {
   withHoogle = true;
@@ -19,6 +19,6 @@ hsPkgs.shellFor {
     ormolu = "latest";
     hlint = "latest";
   };
-  # buildInputs = [ ormolu-wrapped ];
+  # buildInputs = [ ormolu-wrapped ]; # See note about ormolu-wrapped above
   exactDeps = true;
 }
