6c6d3270bd0a56a96e3faa3ad52a2f3073748efa
Jonas Carpay <jonascarpay@gmail.com>
2021-07-07 12:12:04 +0900

    bump haskell.nix

--- pkgs.nix
+++ pkgs.nix
@@ -17,8 +17,8 @@ let
   haskellNix =
     let
       # 2021-04-13
-      commit = "0057d59cffdaed7fa7475f9d6e9a6e84064b6870";
-      sha256 = "07wsgvaarvbl043nwpliq7mygasa16lrrwnc5nyz2j8anpdd4jq4";
+      commit = "446619fe9e481e58df78330957247614224ce7c7";
+      sha256 = "0gi2vvasncms6a5qkb4rklsgsfn8rd69y4h2cimfwy6lblwjsbs2";
     in
     import
       (builtins.fetchTarball {
