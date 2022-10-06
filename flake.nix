8487bbe691babb1e4b551ec00bc41673be6230e8
Jonas Carpay <jonascarpay@gmail.com>
2022-10-06 14:18:21 +0900

    flake: fix nixpkgs to latest release

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "PKGNAME";
 
-  inputs.nixpkgs = { };
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-22.05";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = inputs:
