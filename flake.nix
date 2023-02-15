2ba5b0ed6f396020e03650d112ba58d556cb6fcb
Jonas Carpay <jonascarpay@gmail.com>
2023-02-15 15:09:35 +0900

    Nixpkgs 22.11 bump

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "PKGNAME";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-22.05";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-22.11";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = inputs:
